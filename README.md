c230a3035837b1acda777fa3f2ee1408d012e357
Daniel Lu <daniel1211.lu@gmail.com>
2025-06-23 15:36:32 -0400

    feat: upload init files

---
+++ README.md
@@ -0,0 +1,15 @@
+# clitris
+
+## Getting Started (Mac)
+
+### Homebrew
+
+```sh
+brew install FinityFly/tap/clitris --force
+```
+
+### Build
+
+```sh
+brew install --build-from-source ./clitris.rb
+```
---
+++ clitris.rb
@@ -0,0 +1,18 @@
+class Clitris < Formula
+  desc "Command Line Interface Tetris"
+  homepage "https://github.com/FinityFly/clitris"
+  url "https://github.com/FinityFly/clitris/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "43f7ac337d0493ce874451e123b020f8d649a4bd747eb6f44f93ec1b55a618cb"
+  license "MIT"
+
+  uses_from_macos "ncurses"
+
+  def install
+    system "make"
+    bin.install "clitris"
+  end
+
+  test do
+    assert_match "usage", shell_output("#{bin}/tetris --help 2>&1", 1)
+  end
+end
