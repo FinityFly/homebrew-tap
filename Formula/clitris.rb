22ba1a14bd39eb77699b558ddbfc48966cdcac6b
FinityFly <daniel1211.lu@gmail.com>
2025-06-26 19:17:37 +0000

    Update clitris formula for v0.2.1

--- Formula/clitris.rb
+++ Formula/clitris.rb
@@ -1,8 +1,8 @@
 class Clitris < Formula
   desc "Command Line Interface Tetris"
   homepage "https://github.com/FinityFly/clitris"
-  url "https://github.com/FinityFly/clitris/releases/download/v0.2.0/clitris-v0.2.0.tar.gz"
-  sha256 "5da414516da077529f5a990c85b72b291c3a5aebadfedb5f5e11297857fe1620"
+  url "https://github.com/FinityFly/clitris/releases/download/v0.2.1/clitris-v0.2.1.tar.gz"
+  sha256 "02b23c41ee3d0cc01c98f827ce755612ab7f9c18b8f2dd236424e4ce9fbe427b"
   license "MIT"
 
   uses_from_macos "ncurses"
